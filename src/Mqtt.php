b3f57c200d55965d0b84db82a1b6206752d8e909
Matija Belec <matijabelec1@gmail.com>
2017-08-10 09:09:47 +0200

    add more methods; update unit tests

--- src/Mqtt.php
+++ src/Mqtt.php
@@ -3,8 +3,40 @@
 namespace MatijaBelec\Mqtt;
 
 class Mqtt {
+  private $connection;
+
+  public function __construct()
+  {
+    $this->connection = false;
+  }
+
+  public function connect($options)
+  {
+    $this->connection = true;
+  }
+
+  public function disconnect()
+  {
+    $this->connection = false;
+  }
+
+  public function subscribe($topic)
+  {
+
+  }
+
+  public function unsubscribe($topic)
+  {
+
+  }
+
+  public function publish($topic, $payload)
+  {
+
+  }
+
   public function isConnected()
   {
-    return false;
+    return $this->connection !== false;
   }
 }
--- tests/MqttTest.php
+++ tests/MqttTest.php
@@ -3,9 +3,16 @@
 use MatijaBelec\Mqtt\Mqtt;
 
 class MqttTest extends PHPUnit_Framework_TestCase {
-  public function testMqttIsConnected()
+  public function testMqttConnectAndDisconnect()
   {
     $mqtt = new Mqtt;
-    $this->assertNotTrue($mqtt->isConnected());
+    $options = array(
+      'host' => 'localhost',
+      'port' => 9001,
+    );
+    $mqtt->connect($options);
+    $this->assertTrue($mqtt->isConnected(), 'Connection not established');
+    $mqtt->disconnect();
+    $this->assertNotTrue($mqtt->isConnected(), 'Disconnect failed');
   }
 }
